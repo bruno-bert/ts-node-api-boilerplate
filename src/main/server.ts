11832182a922bfbb81e761b9f10c34e684553f30
Bruno Bertoni de Paula <bruno.bert.jj@gmail.com>
2021-05-31 20:44:21 -0300

    feat: add express server

---
+++ src/main/server.ts
@@ -0,0 +1,11 @@
+import cors from 'cors'
+import express from 'express'
+import { AddressInfo } from 'net'
+const app = express()
+app.use(cors())
+app.get('/', (req, res) => res.send('Hello World!'))
+const port = process.env.PORT ?? 5000
+const server = app.listen(port as number, '0.0.0.0', () => {
+  const { port, address } = server.address() as AddressInfo
+  console.log('Server listening on:',`http://${address}:${port}`)
+})
