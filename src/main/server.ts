71b7a66943e0c478fe4785be88aa6120912406d2
Bruno Bertoni de Paula <bruno.bert.jj@gmail.com>
2021-05-31 22:55:02 -0300

    feat: add static files to express

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,16 @@
 import 'module-alias/register'
-
+import { resolve } from 'path'
 import cors from 'cors'
 import express from 'express'
 import { AddressInfo } from 'net'
+
 const app = express()
+
 app.use(cors())
+app.use('/static', express.static(resolve(__dirname, '../static')))
+
 app.get('/', (req, res) => res.send('Hello Test!'))
+
 const port = process.env.PORT ?? 5000
 const server = app.listen(port as number, '0.0.0.0', () => {
   const { port, address } = server.address() as AddressInfo
